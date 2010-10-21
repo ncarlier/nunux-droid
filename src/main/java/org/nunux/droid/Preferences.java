0b0fd58afe473ed64dfe9301d1d5e53f7f68e40a
pouic <pouic@edf8e8f5-bfa4-44ba-a0b7-e46a8eb9581c>
2010-10-21 15:40:59 +0000

    Fighting with preferences...
    git-svn-id: http://svn.nunux.org/projects/droid/trunk@11 edf8e8f5-bfa4-44ba-a0b7-e46a8eb9581c

--- src/main/java/org/nunux/droid/Preferences.java
+++ src/main/java/org/nunux/droid/Preferences.java
@@ -7,10 +7,13 @@ package org.nunux.droid;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import org.nunux.droid.service.XmppService;
+import org.nunux.droid.tools.ServiceToolkit;
 
 /**
  *
@@ -35,39 +38,59 @@ public class Preferences extends PreferenceActivity
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesName(SHARED_PREFERENCE_NAME);
         addPreferencesFromResource(R.xml.preferences);
-        // Set up a listener whenever a key changes
-        getPreferenceScreen().getSharedPreferences()
-                .registerOnSharedPreferenceChangeListener(this);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+        CheckBoxPreference cbp = (CheckBoxPreference) getPreferenceScreen().findPreference(KEY_ACTIVATION_PREFERENCE);
+        cbp.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Boolean activating = (Boolean) newValue;
+                if (activating.booleanValue()) {
+                    stopService(new Intent(getApplicationContext(), XmppService.class));
+                    startService(new Intent(getApplicationContext(), XmppService.class));
+                } else {
+                    stopService(new Intent(getApplicationContext(), XmppService.class));
+                }
+                return true;
+            }
+        });
+        cbp.setChecked(ServiceToolkit.isServiceExisted(this, XmppService.class.getName()) != null);
 
         for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
             Preference pref = getPreferenceScreen().getPreference(i);
             if (pref instanceof EditTextPreference) {
                 EditTextPreference etp = (EditTextPreference) pref;
                 pref.setSummary(etp.getText());
+            } else if (pref instanceof ListPreference) {
+                ListPreference lp = (ListPreference) pref;
+                pref.setSummary(lp.getValue());
             }
         }
+
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        getPreferenceManager().getSharedPreferences().
+                unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (KEY_ACTIVATION_PREFERENCE.equals(key) && sharedPreferences.getBoolean(key, false)) {
-            if (sharedPreferences.getBoolean(key, false)) {
-                stopService(new Intent(getApplicationContext(), XmppService.class));
-                startService(new Intent(getApplicationContext(), XmppService.class));
-            } else {
-                stopService(new Intent(getApplicationContext(), XmppService.class));
-            }
-        } else {
-            Preference pref = findPreference(key);
-            if (pref instanceof EditTextPreference) {
-                EditTextPreference etp = (EditTextPreference) pref;
-                pref.setSummary(etp.getText());
-            }
+        Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference etp = (EditTextPreference) pref;
+            pref.setSummary(etp.getText());
+        } else if (pref instanceof ListPreference) {
+            ListPreference lp = (ListPreference) pref;
+            pref.setSummary(lp.getValue());
         }
     }
 
