c4c6053e4baab8bef58337e7c31a537441562f7f
pouic <pouic@edf8e8f5-bfa4-44ba-a0b7-e46a8eb9581c>
2010-10-21 15:42:43 +0000

    Fighting with preferences...
    git-svn-id: http://svn.nunux.org/projects/droid/trunk@14 edf8e8f5-bfa4-44ba-a0b7-e46a8eb9581c

---
+++ src/main/java/org/nunux/droid/tools/ServiceToolkit.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.nunux.droid.tools;
+
+import android.app.ActivityManager;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import java.util.List;
+
+/**
+ *
+ * @author fr23972
+ */
+public class ServiceToolkit {
+
+    private ServiceToolkit() {
+    }
+
+    public static ComponentName isServiceExisted(Context context, String className) {
+        ActivityManager activityManager =
+                (ActivityManager) context.getSystemService(Service.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningServiceInfo> serviceList =
+                activityManager.getRunningServices(Integer.MAX_VALUE);
+
+        if (!(serviceList.size() > 0)) {
+            return null;
+        }
+
+        for(int i = 0; i < serviceList.size(); i++) {
+            ActivityManager.RunningServiceInfo serviceInfo = serviceList.get(i);
+            ComponentName serviceName = serviceInfo.service;
+            if(serviceName.getClassName().equals(className)) {
+                return serviceName;
+            }
+        }
+        return null;
+    }
+
+}
